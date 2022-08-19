290a665694b83d39e83d82d8931ee5c26f6b1180
kumo829 <amonmar2000@hotmail.com>
2022-08-18 22:00:48 -0800

    ➕ADD: Basic Scala config and db connection classes

---
+++ src/main/scala/shopping/cart/Main.scala
@@ -0,0 +1,28 @@
+package shopping.cart
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.ActorSystem
+import akka.management.cluster.bootstrap.ClusterBootstrap
+import akka.management.scaladsl.AkkaManagement
+import org.slf4j.LoggerFactory
+import scala.util.control.NonFatal
+
+object Main {
+  val logger = LoggerFactory.getLogger(Main.getClass)
+
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem[Nothing](Behaviors.empty, "ShoppingCartService")
+    try {
+      init(system)
+    }catch {
+      case NonFatal(e) =>
+        logger.error("Terminating due to initialization failure", e)
+        system.terminate()
+    }
+  }
+
+  def init(system: ActorSystem[_]): Unit = {
+    AkkaManagement(system).start()
+    ClusterBootstrap(system).start()
+  }
+}
